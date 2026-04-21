db20b3584c75cf317d7257bccff13ad93bb29f78
ChumaSkymax <godblesschuma720@gmail.com>
2026-04-21 23:03:30 +0300

    fix some issues

--- app/schema/teamDataSchema.ts
+++ app/schema/teamDataSchema.ts
@@ -1,10 +1,9 @@
 import z from "zod";
 
 const teamDataSchema = z.object({
-  _id: z.string().min(3, "ID must be at least 3 characters long"),
   name: z.string().min(3, "Name must be at least 3 characters long"),
-  role: z.string().min(3, "Role must be at least 3 characters long"),
-  image: z.string().url({ message: "Image must be a URL" }),
+  position: z.string().min(3, "Position must be at least 3 characters long"),
+  profileImage: z.instanceof(File, { message: "Profile Image is Required" }),
 });
 
 export default teamDataSchema;
