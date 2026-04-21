f68ef690caaa8349a36747898c59a2132324ad9e
ChumaSkymax <godblesschuma720@gmail.com>
2026-04-21 22:30:42 +0300

    Fix some issues

--- app/schema/legalUpdatesSchema.ts
+++ app/schema/legalUpdatesSchema.ts
@@ -4,6 +4,7 @@ export const legalUpdatesSchema = z.object({
   _id: z.string().optional(),
   title: z.string().min(3, "Title must be at least 3 characters long"),
   slug: z.string().min(3, "Slug must be at least 3 characters long"),
+  author: z.string().min(3, "Author must be at least 3 characters long").optional(),
   description: z
     .string()
     .min(3, "Description must be at least 3 characters long"),
